16000e618718b85e4bc9e88cfb346435d52b8586
Shoxruh <codershoxruh@gmail.com>
2024-03-06 22:36:11 +0500

    Change

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 export const $filter = (array: any[], text: string, key: string) => {
   const txt = text.toLowerCase();
   return array.filter((item) => {
-    if (key) {
-      return item[key].toLowerCase().includes(txt);
-    } else {
-      return item.toLowerCase().includes(txt);
-    }
+    return (key ? item[key] : item).toLowerCase().includes(txt);
   });
 };
 
